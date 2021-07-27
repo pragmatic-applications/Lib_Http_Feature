5fc334d821d57d986d03d4db1c53655ea6d3ba2d
Maurice Kelly <kay86@live.com>
2021-07-27 17:34:02 +0100

    Add docs folder and other files

--- docs/Record.cs
+++ docs/Record.cs
@@ -1,11 +1,11 @@
 //=================================================================
-// Solution: Lib_Generic_Crud_Interface
+// Solution: Lib_Http_Feature
 //=================================================================
 // Programmer: Maurice Antonio Kelly @MAKMaurice
 // Description: A To Do App with undo, redo and database CRUD features.
 // ASP.NET Core 5.0
 // Date: 2021-07-27
-// Stop: 16:00
+// Stop: 18:00
 //=================================================================
 
 
